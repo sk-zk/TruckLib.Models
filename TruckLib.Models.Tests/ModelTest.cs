bb8c698d29480e33f57af6242b1bce5ff646fd61
sk-zk <sk-zk@users.noreply.github.com>
2025-05-20 01:01:33 +0200

    Add test

---
+++ TruckLib.Models.Tests/ModelTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TruckLib.Models;
+
+namespace TruckLib.Models.Tests
+{
+    public class ModelTest
+    {
+        private byte[] pmdBuffer;
+        private byte[] pmgBuffer;
+
+        public ModelTest()
+        {
+            pmdBuffer = File.ReadAllBytes("Data/ModelTest/admin_04_blkw.pmd");
+            pmgBuffer = File.ReadAllBytes("Data/ModelTest/admin_04_blkw.pmg");
+        }
+
+        [Fact]
+        public void SerializePmd()
+        {
+            var model = Model.Load(pmdBuffer, pmgBuffer);
+
+            using var ms = new MemoryStream();
+            using var w = new BinaryWriter(ms);
+            model.SerializePmd(w);
+
+            Assert.Equal(pmdBuffer, ms.ToArray());
+        }
+
+        [Fact]
+        public void SerializePmg()
+        {
+            var model = Model.Load(pmdBuffer, pmgBuffer);
+
+            using var ms = new MemoryStream();
+            using var w = new BinaryWriter(ms);
+            model.SerializePmg(w);
+
+            Assert.Equal(pmgBuffer, ms.ToArray());
+        }
+    }
+}
